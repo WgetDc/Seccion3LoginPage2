63cf131aeffa2c4ade19f80c459e6d155ed5eead
WgetDc <ingcorellana@gmail.com>
2020-10-09 21:16:07 -0300

    Second commit

--- app/src/main/java/com/example/seccion3loginpage2/MainActivity.java
+++ app/src/main/java/com/example/seccion3loginpage2/MainActivity.java
@@ -2,17 +2,39 @@ package com.example.seccion3loginpage2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtMain_email, edtMain_password;
+    //EditText edtMain_password;
+    String correo = "carlos@mail.cl";
+    String clave = "carlos123";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        edtMain_email = (EditText)findViewById(R.id.edtMain_email);
+        edtMain_password = (EditText)findViewById(R.id.edtMain_password);
+
         if (getSupportActionBar() != null){
             getSupportActionBar().hide();
+        }
+    }
+
+    public void goToHomePage(View view){
+        if (correo.equals(edtMain_email.getText().toString()) && clave.equals(edtMain_password.getText().toString())){
+            Intent activity = new Intent(this, HomeActivity.class);
+            startActivity(activity);
+        }else {
+            Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
         }
     }
 }
