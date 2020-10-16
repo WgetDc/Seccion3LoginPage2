23ce785def6f2c626e521baecb8d199bf24f8ae5
WgetDc <ingcorellana@gmail.com>
2020-10-16 20:50:19 -0300

    Clase del viernes 16 de octubre

--- app/src/main/java/com/example/seccion3loginpage2/MainActivity.java
+++ app/src/main/java/com/example/seccion3loginpage2/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.seccion3loginpage2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,31 +9,46 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText edtMain_email, edtMain_password;
-    //EditText edtMain_password;
-    String correo = "carlos@mail.cl";
+    String correo = "ingcorellana@gmail.com";
     String clave = "carlos123";
+
+    EditText edtMain_email;
+    EditText edtMain_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        edtMain_email = (EditText)findViewById(R.id.edtMain_email);
-        edtMain_password = (EditText)findViewById(R.id.edtMain_password);
-
+        //Oculta el action bar
         if (getSupportActionBar() != null){
             getSupportActionBar().hide();
         }
+
+        edtMain_email = (EditText) findViewById(R.id.edtMain_email);
+        edtMain_password = findViewById(R.id.edtMain_password);
+
     }
 
-    public void goToHomePage(View view){
-        if (correo.equals(edtMain_email.getText().toString()) && clave.equals(edtMain_password.getText().toString())){
+    public void goToHomeActivity(View view){
+        String getEmail = edtMain_email.getText().toString();
+        String getPassword = edtMain_password.getText().toString();
+        if(correo.equals(getEmail) && clave.equals(getPassword)){
             Intent activity = new Intent(this, HomeActivity.class);
             startActivity(activity);
+            Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show();
         }else {
             Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
         }
+    /*
+        String getEmail = edtMain_email.getText().toString();
+        String getPassword = edtMain_password.getText().toString();
+
+        Toast.makeText(this, "Su correo es: " + getEmail, Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(this, "Su contraseña es: " + getPassword, Toast.LENGTH_SHORT).show();
+
+    */
     }
+
+
 }
